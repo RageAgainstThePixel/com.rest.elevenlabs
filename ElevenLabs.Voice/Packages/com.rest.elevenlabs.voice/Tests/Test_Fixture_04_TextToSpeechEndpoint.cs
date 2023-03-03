8f88365b3231d0fbacf2cbabd001ace5c9b23961
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-03 14:05:24 -0500

    removed demo code that won't be used
    updated text to speech test

--- ElevenLabs.Voice/Packages/com.rest.elevenlabs.voice/Tests/Test_Fixture_04_TextToSpeechEndpoint.cs
+++ ElevenLabs.Voice/Packages/com.rest.elevenlabs.voice/Tests/Test_Fixture_04_TextToSpeechEndpoint.cs
@@ -1,7 +1,11 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using ElevenLabs;
+using NUnit.Framework;
+using System;
 using System.Collections;
-using System.Threading.Tasks;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Rest.ElevenLabs.Voice.Tests
@@ -13,17 +17,25 @@ namespace Rest.ElevenLabs.Voice.Tests
         {
             yield return AwaitTestUtilities.Await(async () =>
             {
-                await Task.CompletedTask;
+                var api = new ElevenLabsClient(ElevenLabsAuthentication.LoadFromEnv());
+                Assert.NotNull(api.TextToSpeechEndpoint);
+                var voice = (await api.VoicesEndpoint.GetAllVoicesAsync()).FirstOrDefault();
+                Assert.NotNull(voice);
+                var defaultVoiceSettings = await api.VoicesEndpoint.GetDefaultVoiceSettingsAsync();
+                var (clipPath, audioClip) = await api.TextToSpeechEndpoint.TextToSpeechAsync("The quick brown fox jumps over the lazy dog.", voice, defaultVoiceSettings);
+                Assert.NotNull(audioClip);
+                Debug.Log(clipPath);
             });
         }
 
-        [UnityTest]
-        public IEnumerator Test_02_TextToSpeechStream()
-        {
-            yield return AwaitTestUtilities.Await(async () =>
-            {
-                await Task.CompletedTask;
-            });
-        }
+        //[UnityTest]
+        //public IEnumerator Test_02_TextToSpeechStream()
+        //{
+        //    yield return AwaitTestUtilities.Await(async () =>
+        //    {
+        //        TODO
+        //        await Task.CompletedTask;
+        //    });
+        //}
     }
 }
