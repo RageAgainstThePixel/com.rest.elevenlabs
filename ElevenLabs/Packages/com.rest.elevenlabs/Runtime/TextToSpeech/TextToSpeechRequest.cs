8cc93bd165de43774a66ea2ff462d82e43b3eb12
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-11-24 23:56:00 -0500

    docs

--- ElevenLabs/Packages/com.rest.elevenlabs/Runtime/TextToSpeech/TextToSpeechRequest.cs
+++ ElevenLabs/Packages/com.rest.elevenlabs/Runtime/TextToSpeech/TextToSpeechRequest.cs
@@ -12,6 +12,47 @@ namespace ElevenLabs.TextToSpeech
     [Preserve]
     public sealed class TextToSpeechRequest
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="voice">
+        /// <see cref="Voice"/> to use.
+        /// </param>
+        /// <param name="text">
+        /// Text input to synthesize speech for. Maximum 5000 characters.
+        /// </param>
+        /// <param name="encoding"><see cref="Encoding"/> to use for <see cref="text"/>.</param>
+        /// <param name="voiceSettings">
+        /// Optional, <see cref="VoiceSettings"/> that will override the default settings in <see cref="Voice.Settings"/>.
+        /// </param>
+        /// <param name="model">
+        /// Optional, <see cref="Model"/> to use. Defaults to <see cref="Model.TurboV2_5"/>.
+        /// </param>
+        /// <param name="outputFormat">
+        /// Output format of the generated audio.<br/>
+        /// Defaults to <see cref="OutputFormat.MP3_44100_128"/>
+        /// </param>
+        /// <param name="optimizeStreamingLatency">
+        /// Optional, You can turn on latency optimizations at some cost of quality.
+        /// The best possible final latency varies by model.<br/>
+        /// Possible values:<br/>
+        /// 0 - default mode (no latency optimizations)<br/>
+        /// 1 - normal latency optimizations (about 50% of possible latency improvement of option 3)<br/>
+        /// 2 - strong latency optimizations (about 75% of possible latency improvement of option 3)<br/>
+        /// 3 - max latency optimizations<br/>
+        /// 4 - max latency optimizations, but also with text normalizer turned off for even more latency savings
+        /// (best latency, but can mispronounce e.g. numbers and dates).
+        /// </param>
+        /// <param name="previousText"></param>
+        /// <param name="nextText"></param>
+        /// <param name="previousRequestIds"></param>
+        /// <param name="nextRequestIds"></param>
+        /// <param name="languageCode">
+        /// Optional, Language code (ISO 639-1) used to enforce a language for the model. Currently only <see cref="Model.TurboV2_5"/> supports language enforcement. 
+        /// For other models, an error will be returned if language code is provided.
+        /// </param>
+        /// <param name="cacheFormat"></param>
+        /// <param name="withTimestamps"></param>
         [Preserve]
         public TextToSpeechRequest(
             Voice voice,
